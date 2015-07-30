b49ce0ca85dd03978272768472babd5cade4ff63
Erica Irving <contact.eirving@gmail.com>
2015-07-29 20:09:57 -0400

    create view and controller for posts new

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,11 +9,12 @@ class PostsController < ApplicationController
   end
 
   def new
-
+    @post = Post.new
   end
 
   def create
-
+    @post = Post.create!(post_params)
+    redirect_to post_path(@post)
   end
 
   def edit
